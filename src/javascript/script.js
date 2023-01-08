82031dd0903e8944ea2d74d6c5066fb2d0e7aace
Florian Houtmann <105522552+floh68@users.noreply.github.com>
2023-01-08 19:14:39 +0100

    carousel fini

--- src/javascript/script.js
+++
@@ -1,7 +0,0 @@
-const articlesLink = document.querySelector('.dropdown > a');
-const dropdownMenu = document.querySelector('.dropdown-menu');
-
-articlesLink.addEventListener('click', function(event) {
-  event.preventDefault();
-  dropdownMenu.classList.toggle('show');
-});
